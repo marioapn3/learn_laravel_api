7292fedf460c17eeee88c19d847321bea0d9f311
marioapn3 <mario.aprilnino27@gmail.com>
2023-07-31 00:27:17 +0700

    learn Resource

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostDetailResoure;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -10,8 +12,21 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
+        //! JIKA HASIL LEBIH DARI 1 MAKA MENGGUNAKAN COLLECTION , NAMUN JIKA HASIL 1 MAKA PAKAI NEW "nm resource($data)"
+        return PostResource::collection($posts);
+    }
 
+    public function show($id)
+    {
+        $post = Post::with('writer:id,username')->findOrFail($id);
+        //? kenapa ga pakai collection??
+        //! Karena kalau hanya 1 array yang ditampilkan maka tidak bisa
+        return new PostDetailResoure($post);
+    }
 
-        return response()->json($posts);
+    public function show2($id)
+    {
+        $post = Post::findOrFail($id);
+        return new PostDetailResoure($post);
     }
 }
---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+
+    public function toArray(Request $request): array
+    {
+        $created_at = date_format($this->created_at, "Y/m/d H:i:s");
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'news_content' => $this->news_content,
+            'created_at' => $created_at
+
+        ];
+    }
+}
